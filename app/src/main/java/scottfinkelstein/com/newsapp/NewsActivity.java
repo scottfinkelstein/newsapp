9c71de2394185a4e5f8191e0b5c159cb19bcc537
finkelstein <finkelstein@adelphi.edu>
2018-10-12 08:19:08 -0400

    Changed JSON Key names to demo serializing names.

--- app/src/main/java/scottfinkelstein/com/newsapp/NewsActivity.java
+++ app/src/main/java/scottfinkelstein/com/newsapp/NewsActivity.java
@@ -17,34 +17,34 @@ public class NewsActivity extends AppCompatActivity {
 
     private String newsJson = "{\n" +
             "\t\"data\": [{\n" +
-            "\t\t\"title\": \"Adelphi President, Alumnae Recognized Among Top 50 Most Influential Women In Business\",\n" +
-            "\t\t\"pubDate\": \"2018-10-05T16:30:00-04:00\",\n" +
-            "\t\t\"desc\": \"Long Island Business News\\u00a0has named\\u00a0Adelphi University President Christine Riordan one of the Top 50 Most Influential Women in Business.\"\n" +
+            "\t\t\"news_title\": \"Adelphi President, Alumnae Recognized Among Top 50 Most Influential Women In Business\",\n" +
+            "\t\t\"pub_date\": \"2018-10-05T16:30:00-04:00\",\n" +
+            "\t\t\"description\": \"Long Island Business News\\u00a0has named\\u00a0Adelphi University President Christine Riordan one of the Top 50 Most Influential Women in Business.\"\n" +
             "\t\t\n" +
             "\t}, {\n" +
-            "\t\t\"title\": \"Mel Pender '97 (Hon.), Continues to Go for the Gold\",\n" +
-            "\t\t\"pubDate\": \"2018-10-04T10:38:00-04:00\",\n" +
-            "\t\t\"desc\": \"He's an Olympic champion, military hero, entrepreneur and philanthropist, and on Saturday, October 13, Mel Pender '76, '97 (Hon.), will add another title to his accomplishments: Adelphi Legend.\"\n" +
+            "\t\t\"news_title\": \"Mel Pender '97 (Hon.), Continues to Go for the Gold\",\n" +
+            "\t\t\"pub_date\": \"2018-10-04T10:38:00-04:00\",\n" +
+            "\t\t\"description\": \"He's an Olympic champion, military hero, entrepreneur and philanthropist, and on Saturday, October 13, Mel Pender '76, '97 (Hon.), will add another title to his accomplishments: Adelphi Legend.\"\n" +
             "\t\t\n" +
             "\t}, {\n" +
-            "\t\t\"title\": \"In Adelphi's Creative Writing M.F.A. Program, Finding Your Voice Is a Group Effort\",\n" +
-            "\t\t\"pubDate\": \"2018-10-03T15:42:00-04:00\",\n" +
-            "\t\t\"desc\": \"What is the right class size for graduate work in creative writing? Igor Webb, Ph.D., professor and director of the M.F.A. in Creative Writing program at Adelphi, believes strongly that the answer is 10 students.\"\n" +
+            "\t\t\"news_title\": \"In Adelphi's Creative Writing M.F.A. Program, Finding Your Voice Is a Group Effort\",\n" +
+            "\t\t\"pub_date\": \"2018-10-03T15:42:00-04:00\",\n" +
+            "\t\t\"description\": \"What is the right class size for graduate work in creative writing? Igor Webb, Ph.D., professor and director of the M.F.A. in Creative Writing program at Adelphi, believes strongly that the answer is 10 students.\"\n" +
             "\t\n" +
             "\t}, {\n" +
-            "\t\t\"title\": \"Faerie Knitting by Alice Hoffman is Here to Enchant Readers and Knitters\",\n" +
-            "\t\t\"pubDate\": \"2018-10-03T14:01:00-04:00\",\n" +
-            "\t\t\"desc\": \"Alice Hoffman '73, '02 (Hon.), our 2018\\u00a0Legends Dinner\\u00a0keynote speaker, has written fiction for children, middle-grade books, adult novels and a work of nonfiction. A thread of magic runs through many of them, but her latest is quite a departure from its predecessors.\"\n" +
+            "\t\t\"news_title\": \"Faerie Knitting by Alice Hoffman is Here to Enchant Readers and Knitters\",\n" +
+            "\t\t\"pub_date\": \"2018-10-03T14:01:00-04:00\",\n" +
+            "\t\t\"description\": \"Alice Hoffman '73, '02 (Hon.), our 2018\\u00a0Legends Dinner\\u00a0keynote speaker, has written fiction for children, middle-grade books, adult novels and a work of nonfiction. A thread of magic runs through many of them, but her latest is quite a departure from its predecessors.\"\n" +
             "\t\t\n" +
             "\t}, {\n" +
-            "\t\t\"title\": \"Adelphi Kicks Off Spirit Weekend With First-Ever Fall Frenzy Concert\",\n" +
-            "\t\t\"pubDate\": \"2018-10-03T13:21:00-04:00\",\n" +
-            "\t\t\"desc\": \"Adelphi's second annual Spirit Weekend has a first: the inaugural\\u00a0Fall Frenzy concert.\"\n" +
+            "\t\t\"news_title\": \"Adelphi Kicks Off Spirit Weekend With First-Ever Fall Frenzy Concert\",\n" +
+            "\t\t\"pub_date\": \"2018-10-03T13:21:00-04:00\",\n" +
+            "\t\t\"description\": \"Adelphi's second annual Spirit Weekend has a first: the inaugural\\u00a0Fall Frenzy concert.\"\n" +
             "\t\t\n" +
             "\t}, {\n" +
-            "\t\t\"title\": \"Meet the New Director of the Center for Student Involvement\",\n" +
-            "\t\t\"pubDate\": \"2018-10-03T12:47:00-04:00\",\n" +
-            "\t\t\"desc\": \"The Center for Student Involvement (CSI) has a new director, and she's no stranger to Adelphi.\"\n" +
+            "\t\t\"news_title\": \"Meet the New Director of the Center for Student Involvement\",\n" +
+            "\t\t\"pub_date\": \"2018-10-03T12:47:00-04:00\",\n" +
+            "\t\t\"description\": \"The Center for Student Involvement (CSI) has a new director, and she's no stranger to Adelphi.\"\n" +
             "\t}]\n" +
             "}";
 
